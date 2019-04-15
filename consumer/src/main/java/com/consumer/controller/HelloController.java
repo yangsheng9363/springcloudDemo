df753aa49a646eda48464e28199c24b6a7614c38
coderyang <18740448749@163.com>
2019-04-15 09:58:10 +0800

    123

---
+++ consumer/src/main/java/com/consumer/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.consumer.controller;
+
+
+import com.consumer.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name)
+    {
+        return helloService.hiService(name);
+    }
+
+}
